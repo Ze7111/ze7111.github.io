156928859a091876ba4ccab448097a03469fd756
Dhruvan <dhruvan.k05@gmail.com>
2022-09-25 20:17:45 +0400

    final

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,7 +29,14 @@ function Home() {
                 <Type />
               </div>
             </Col>
-
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
           </Row>
         </Container>
       </Container>
