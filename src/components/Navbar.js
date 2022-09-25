7a736cf50ea19c5976b8b042614ab9d74d2a9e9a
Dhruvan <dhruvan.k05@gmail.com>
2022-09-25 18:38:58 +0400

    almost final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://blogs.soumya-jit.tech/"
+                href="not available"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Ze7111/ze7111.github.io"
                 target="_blank"
                 className="fork-btn-inner"
               >
